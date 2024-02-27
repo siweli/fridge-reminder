682dd90922dbef9192f5e25c5e18f97b11bbbd48
siweli <lewisr.py@gmail.com>
2024-02-27 11:14:51 +0000

    minor change

--- fridge-reminder-website/prisma/index.js
+++ fridge-reminder-website/prisma/index.js
@@ -1,9 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
-
 const prisma = new PrismaClient()
 
 async function main() {
-
   await prisma.users.create({
     data: {
       name: 'Lewis',
